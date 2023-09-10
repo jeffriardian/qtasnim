a4c602c7b4fe479901ac925373ca6c1caba924b3
jeffriardian <60500344+jeffriardian@users.noreply.github.com>
2023-09-10 23:49:32 +0700

    models and seeders

---
+++ database/seeders/JenisBarangSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Jenis;
+use Datetime;
+use DB;
+
+class JenisBarangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $jenis = [
+            [
+                'nama' => "Konsumsi",
+                'created_at' => new DateTime(),
+                'updated_at' => null,
+            ],
+            [
+                'nama' => "Pembersih",
+                'created_at' => new DateTime(),
+                'updated_at' => null,
+            ],
+        ];
+
+        DB::table('jenis')->insert($jenis);
+    }
+}
